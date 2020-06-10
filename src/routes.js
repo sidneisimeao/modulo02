06a0629e65859bb96254d805826c95a1d58b90e2
Sidnei Simeão <sidneisimmon@gmail.com>
2020-06-10 18:11:01 -0300

    11 - Cadastro de usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserContrller';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Sidnei Simeão',
-    email: 'sidneisimmon@gmail.com',
-    password_hash: '123456',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
