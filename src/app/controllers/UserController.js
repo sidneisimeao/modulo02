ff4eec910d52f59b657195a924c3341e71889a2f
Sidnei Simeão <sidneisimmon@gmail.com>
2020-06-11 11:28:06 -0300

    16 - Update perfil usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,6 +16,38 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    // Se tiver alteração de email
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      // Verifica se outro usuário já não esta usando o e-mail
+      if (userExists) {
+        return res.status(400).json({
+          error: 'Este e-mail já esta sendo utilizado por outro usuário',
+        });
+      }
+    }
+
+    // Se tiver alteração de senha
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Senha inválida' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
