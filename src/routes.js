c86679b0a132e699e36e0f308a1c96bd7823e1d3
Sidnei Simeão <sidneisimmon@gmail.com>
2020-06-11 10:26:22 -0300

    15 - Middleware de autenticaçao

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import UserController from './app/controllers/UserContrller';
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
