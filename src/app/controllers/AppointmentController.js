4c74b74d203d101462ac5e21d0f691bf29c84cf4
Sidnei Simeão <sidneisimmon@gmail.com>
2020-06-17 16:37:01 -0300

    06 - Validaçoes de agendamento

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,71 @@
+import * as yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = yup.object().shape({
+      provider_id: yup.number().required(),
+      date: yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Dados inválidos' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    /**
+     * Check if provider_id is a provider
+     */
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Você só pode criar agendamentos com provedores' });
+    }
+
+    /**
+     * Check for past dates
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res
+        .status(401)
+        .json({ error: 'Datas passadas não são permitidas' });
+    }
+
+    /**
+     * Check date availability
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(401)
+        .json({ error: 'Data do agendamento não disponível' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,7 +19,10 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
